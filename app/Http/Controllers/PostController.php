7ab17ffe266de342e29360c9ae842b13181f4994
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-01 15:48:50 -0700

    change redirect when post a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -143,7 +143,7 @@ class PostController extends Controller
 
         Session::flash('flash_message', 'Post updated!');
 
-        return redirect('master/post');
+        return redirect('post');
     }
 
     /**
