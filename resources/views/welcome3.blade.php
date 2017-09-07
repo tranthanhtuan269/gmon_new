c9fa08868f1df6f5d615d4dd69b7d525477578b7
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-07 16:55:17 -0700

    add slider to welcome 3 page

--- resources/views/welcome3.blade.php
+++ resources/views/welcome3.blade.php
@@ -295,13 +295,129 @@
             </div>
         </div>
     </div>
-    <div class="part-2">
-        <div class="container">
-            <div class="row">
-                <img src="http://test.gmon.com.vn/?image=part-2.jpg" alt="">
-            </div>
-        </div>
+
+    <style type="text/css">
+      * {box-sizing:border-box}
+
+      /* Slideshow container */
+      .slideshow-container {
+        max-width: 1000px;
+        position: relative;
+        margin: auto;
+      }
+
+      .mySlides {
+          display: none;
+      }
+
+      /* Next & previous buttons */
+      .prev, .next {
+        cursor: pointer;
+        position: absolute;
+        top: 50%;
+        width: auto;
+        margin-top: -22px;
+        padding: 16px;
+        color: white;
+        font-weight: bold;
+        font-size: 18px;
+        transition: 0.6s ease;
+        border-radius: 0 3px 3px 0;
+      }
+
+      /* Position the "next button" to the right */
+      .next {
+        right: 0;
+        border-radius: 3px 0 0 3px;
+      }
+
+      /* On hover, add a black background color with a little bit see-through */
+      .prev:hover, .next:hover {
+        background-color: rgba(0,0,0,0.8);
+      }
+
+      /* Caption text */
+      .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+      }
+
+      /* Number text (1/3 etc) */
+      .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+      }
+
+      /* The dots/bullets/indicators */
+      .dot {
+        cursor:pointer;
+        height: 13px;
+        width: 13px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+      }
+
+      .active, .dot:hover {
+        background-color: #717171;
+      }
+
+      /* Fading animation */
+      .fade {
+        -webkit-animation-name: fade;
+        -webkit-animation-duration: 4s;
+        animation-name: fade;
+        animation-duration: 4s;
+      }
+
+      @-webkit-keyframes fade {
+        from {opacity: .6} 
+        to {opacity: 1}
+      }
+
+      @keyframes fade {
+        from {opacity: .6} 
+        to {opacity: 1}
+      }
+    </style>
+    <div class="part-2 slideshow-container">
+      <div class="container mySlides fade">
+        <img src="http://test.gmon.com.vn/?image=U5-01.jpg" style="width:100%">
+      </div>
+
+      <div class="container mySlides fade">
+        <img src="http://test.gmon.com.vn/?image=U5-02.jpg" style="width:100%">
+      </div>
     </div>
+    <br>
+
+    <script type="text/javascript">
+      var slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none"; 
+          }
+          slideIndex++;
+          if (slideIndex> slides.length) {slideIndex = 1} 
+          slides[slideIndex-1].style.display = "block"; 
+          setTimeout(showSlides, 4000); // Change image every 2 seconds
+      }
+    </script>
+
     <div class="part-3">
         <div class="container">
             <div class="wrap">
