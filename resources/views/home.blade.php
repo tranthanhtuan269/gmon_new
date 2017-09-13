4d8973f416f76aa62279c35defd170236cc817d1
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-13 15:42:12 -0700

    change link images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -279,12 +279,10 @@
                           </div>
                         @else
                         <div class="sub-description">
-                            <?php 
-                              echo str_replace("/public/templateEditor/kcfinder/upload/images/","http://gmon.vn/public/templateEditor/kcfinder/upload/images/",$post->sub_description);?>
-                            <a href="{{ url('/') }}/?post={{ $post->id }}">Xem thêm</a>
+                            <?php echo $post->sub_description; ?><a href="{{ url('/') }}/?post={{ $post->id }}">Xem thêm</a>
                         </div>
                         <div class="description" style="display: none;">
-                            <?php echo $post->description; ?>
+                            <?php echo str_replace("/public/templateEditor/kcfinder/upload/images/","http://gmon.vn/public/templateEditor/kcfinder/upload/images/",$post->sub_description); ?>
                         </div>
                         </p>
                         <div class="images">
