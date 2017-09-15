9543a92571aeeb2799d0d12ff8094fccdee63877
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-15 11:18:43 -0700

    add partner

---
+++ app/Http/Controllers/PartnerController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Partner;
+use Illuminate\Http\Request;
+use Session;
+
+class PartnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $partner = Partner::where('link', 'LIKE', "%$keyword%")
+				->orWhere('image', 'LIKE', "%$keyword%")
+				->paginate($perPage);
+        } else {
+            $partner = Partner::paginate($perPage);
+        }
+
+        return view('partner.index', compact('partner'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('partner.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        Partner::create($requestData);
+
+        Session::flash('flash_message', 'Partner added!');
+
+        return redirect('master/partner');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $partner = Partner::findOrFail($id);
+
+        return view('partner.show', compact('partner'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $partner = Partner::findOrFail($id);
+
+        return view('partner.edit', compact('partner'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        $partner = Partner::findOrFail($id);
+        $partner->update($requestData);
+
+        Session::flash('flash_message', 'Partner updated!');
+
+        return redirect('master/partner');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Partner::destroy($id);
+
+        Session::flash('flash_message', 'Partner deleted!');
+
+        return redirect('master/partner');
+    }
+}
