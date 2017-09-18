8fdfaa25da7dda2a3105da881c93071d5b642c07
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-18 10:34:11 -0700

    aaa

--- resources/views/curriculum-vitae/edit_curriculum_vitae.blade.php
+++ resources/views/curriculum-vitae/edit_curriculum_vitae.blade.php
@@ -369,7 +369,7 @@
                                     <input type="hidden" id="lam_viec_0_image" value="">
                                     <div class="form-group">
                                         <div class="col-md-6">
-                                            <label for="ten_cong_ty" class="col-md-4 control-label">Tên công ty đã làm</label>
+                                            <label for="ten_cong_ty" class="col-md-4 control-label">Tên công ty</label>
                                             <div class="col-md-8">
                                                 <input type="text" class="form-control" class="ten_cong_ty_0" id="ten_cong_ty_0">
                                             </div>
@@ -388,25 +388,16 @@
                                         </div>
                                         <div class="col-md-2">
                                             <select class="form-control col-md-2" class="thang_bat_dau" id="thang_bat_dau_lam_viec_0">
-                                                <option value="0">--Chọn Tháng--</option>
-                                                <option value="1">Tháng 1</option>
-                                                <option value="2">Tháng 2</option>
-                                                <option value="3">Tháng 3</option>
-                                                <option value="4">Tháng 4</option>
-                                                <option value="5">Tháng 5</option>
-                                                <option value="6">Tháng 6</option>
-                                                <option value="7">Tháng 7</option>
-                                                <option value="8">Tháng 8</option>
-                                                <option value="9">Tháng 9</option>
-                                                <option value="10">Tháng 10</option>
-                                                <option value="11">Tháng 11</option>
-                                                <option value="12">Tháng 12</option>
+                                                @foreach($months as $key => $value)
+                                                <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                         <div class="col-md-2">
                                             <select class="form-control col-md-2" class="nam_bat_dau" id="nam_bat_dau_lam_viec_0">
-                                                <option value="0">--Chọn Năm--</option>
-                                                <option value="2017">Năm 2017</option><option value="2016">Năm 2016</option><option value="2015">Năm 2015</option><option value="2014">Năm 2014</option><option value="2013">Năm 2013</option><option value="2012">Năm 2012</option><option value="2011">Năm 2011</option><option value="2010">Năm 2010</option><option value="2009">Năm 2009</option><option value="2008">Năm 2008</option><option value="2007">Năm 2007</option><option value="2006">Năm 2006</option><option value="2005">Năm 2005</option><option value="2004">Năm 2004</option><option value="2003">Năm 2003</option><option value="2002">Năm 2002</option><option value="2001">Năm 2001</option><option value="2000">Năm 2000</option><option value="1999">Năm 1999</option><option value="1998">Năm 1998</option><option value="1997">Năm 1997</option><option value="1996">Năm 1996</option><option value="1995">Năm 1995</option><option value="1994">Năm 1994</option><option value="1993">Năm 1993</option><option value="1992">Năm 1992</option><option value="1991">Năm 1991</option><option value="1990">Năm 1990</option><option value="1989">Năm 1989</option><option value="1988">Năm 1988</option><option value="1987">Năm 1987</option><option value="1986">Năm 1986</option><option value="1985">Năm 1985</option><option value="1984">Năm 1984</option><option value="1983">Năm 1983</option><option value="1982">Năm 1982</option><option value="1981">Năm 1981</option><option value="1980">Năm 1980</option><option value="1979">Năm 1979</option><option value="1978">Năm 1978</option><option value="1977">Năm 1977</option><option value="1976">Năm 1976</option><option value="1975">Năm 1975</option><option value="1974">Năm 1974</option><option value="1973">Năm 1973</option><option value="1972">Năm 1972</option><option value="1971">Năm 1971</option><option value="1970">Năm 1970</option><option value="1969">Năm 1969</option><option value="1968">Năm 1968</option><option value="1967">Năm 1967</option><option value="1966">Năm 1966</option><option value="1965">Năm 1965</option><option value="1964">Năm 1964</option><option value="1963">Năm 1963</option><option value="1962">Năm 1962</option><option value="1961">Năm 1961</option>
+                                                @foreach($years as $key => $value)
+                                                <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                         <div class="col-md-1">
@@ -414,44 +405,22 @@
                                         </div>
                                         <div class="col-md-2">
                                             <select class="form-control col-md-2" class="thang_ket_thuc" id="thang_ket_thuc_lam_viec_0">
-                                                <option value="0">--Chọn Tháng--</option>
-                                                <option value="1">Tháng 1</option>
-                                                <option value="2">Tháng 2</option>
-                                                <option value="3">Tháng 3</option>
-                                                <option value="4">Tháng 4</option>
-                                                <option value="5">Tháng 5</option>
-                                                <option value="6">Tháng 6</option>
-                                                <option value="7">Tháng 7</option>
-                                                <option value="8">Tháng 8</option>
-                                                <option value="9">Tháng 9</option>
-                                                <option value="10">Tháng 10</option>
-                                                <option value="11">Tháng 11</option>
-                                                <option value="12">Tháng 12</option>
+                                                @foreach($months as $key => $value)
+                                                <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                         <div class="col-md-2">
                                             <select class="form-control col-md-2" class="nam_ket_thuc" id="nam_ket_thuc_lam_viec_0">
-                                                <option value="0">--Chọn Năm--</option>
-                                                <option value="2017">Năm 2017</option><option value="2016">Năm 2016</option><option value="2015">Năm 2015</option><option value="2014">Năm 2014</option><option value="2013">Năm 2013</option><option value="2012">Năm 2012</option><option value="2011">Năm 2011</option><option value="2010">Năm 2010</option><option value="2009">Năm 2009</option><option value="2008">Năm 2008</option><option value="2007">Năm 2007</option><option value="2006">Năm 2006</option><option value="2005">Năm 2005</option><option value="2004">Năm 2004</option><option value="2003">Năm 2003</option><option value="2002">Năm 2002</option><option value="2001">Năm 2001</option><option value="2000">Năm 2000</option><option value="1999">Năm 1999</option><option value="1998">Năm 1998</option><option value="1997">Năm 1997</option><option value="1996">Năm 1996</option><option value="1995">Năm 1995</option><option value="1994">Năm 1994</option><option value="1993">Năm 1993</option><option value="1992">Năm 1992</option><option value="1991">Năm 1991</option><option value="1990">Năm 1990</option><option value="1989">Năm 1989</option><option value="1988">Năm 1988</option><option value="1987">Năm 1987</option><option value="1986">Năm 1986</option><option value="1985">Năm 1985</option><option value="1984">Năm 1984</option><option value="1983">Năm 1983</option><option value="1982">Năm 1982</option><option value="1981">Năm 1981</option><option value="1980">Năm 1980</option><option value="1979">Năm 1979</option><option value="1978">Năm 1978</option><option value="1977">Năm 1977</option><option value="1976">Năm 1976</option><option value="1975">Năm 1975</option><option value="1974">Năm 1974</option><option value="1973">Năm 1973</option><option value="1972">Năm 1972</option><option value="1971">Năm 1971</option><option value="1970">Năm 1970</option><option value="1969">Năm 1969</option><option value="1968">Năm 1968</option><option value="1967">Năm 1967</option><option value="1966">Năm 1966</option><option value="1965">Năm 1965</option><option value="1964">Năm 1964</option><option value="1963">Năm 1963</option><option value="1962">Năm 1962</option><option value="1961">Năm 1961</option>
+                                                @foreach($years as $key => $value)
+                                                <option value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
                                             </select>
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <div class="col-md-12">
-                                            <label for="ten_cong_ty" class="col-md-2 control-label">Địa chỉ công ty</label>
-                                            <div class="col-md-3">
-                                                <select class="form-control thanh_pho" id="thanh_pho_0"><option value="0">--Chọn Tỉnh / Thành Phố--</option><option value="1">Hà Nội</option><option value="2">Hồ Chí Minh</option><option value="3">Đà Nẵng</option><option value="4">Hải Phòng</option><option value="5">Cần Thơ</option><option value="6">An Giang</option><option value="7">Bà Rịa Vũng Tàu</option><option value="8">Bạc Liêu</option><option value="9">Bắc Cạn</option><option value="10">Bắc Giang</option><option value="11">Hải Dương</option><option value="12">Bắc Ninh</option><option value="13">Bến Tre</option><option value="14">Bình Dương</option><option value="15">Bình Định</option><option value="16">Bình Phước</option><option value="17">Bình Thuận</option><option value="18">Cà Mau</option><option value="19">Cao Bằng</option><option value="20">Đắk Lắk</option><option value="21">Đăk Nông</option><option value="22">Điện Biên</option><option value="23">Đồng Nai</option><option value="24">Đồng Tháp</option><option value="25">Gia Lai</option><option value="26">Hà Giang</option><option value="27">Hà Nam</option><option value="28">Hà Tĩnh</option><option value="29">Hậu Giang</option><option value="30">Hòa Bình</option><option value="31">Hưng Yên</option><option value="32">Khánh Hòa</option><option value="33">Kiên Giang</option><option value="34">Kon Tum</option><option value="35">Lai Châu</option><option value="36">Lâm Đồng</option><option value="37">Lạng Sơn</option><option value="38">Lào Cai</option><option value="39">Long An</option><option value="40">Nam Định</option><option value="41">Nghệ An</option><option value="42">Ninh Bình</option><option value="43">Ninh Thuận</option><option value="44">Phú Thọ</option><option value="45">Phú Yên</option><option value="46">Quảng Bình</option><option value="47">Quảng Nam</option><option value="48">Quảng Ngãi</option><option value="49">Quảng Ninh</option><option value="50">Quảng Trị</option><option value="51">Sóc Trăng</option><option value="52">Sơn La</option><option value="53">Tây Ninh</option><option value="54">Thái Bình</option><option value="55">Thái Nguyên</option><option value="56">Thanh Hóa</option><option value="57">Huế</option><option value="58">Tiền Giang</option><option value="59">Trà Vinh</option><option value="60">Tuyên Quang</option><option value="61">Vĩnh Long</option><option value="62">Vĩnh Phúc</option><option value="63">Yên Bái</option></select>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <select class="form-control quan_huyen" id="quan_huyen_0"><option value="0">--Chọn Quận / Huyện --</option></select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="col-md-2 col-sm-offset-1 image_company">
-                                            <input type="file" class="company-img" id="company-img-0" style="display: none;">
-                                        </div>
-                                        <div class="col-md-8 col-sm-offset-1">
+                                        <label for="ten_cong_ty" class="col-md-2 control-label">Mô tả công việc</label>
+                                        <div class="col-md-10">
                                             <textarea rows="4" cols="50" class="form-control" class="mo_ta_0" id="mo_ta_0" placeholder="Mô tả ngắn về công việc"></textarea>
                                         </div>
                                     </div>
@@ -466,6 +435,7 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
                             <?php 
                             }
                             ?>
