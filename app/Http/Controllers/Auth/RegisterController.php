44007b248015e70ef101927825016173c2c2157d
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-30 11:43:45 -0700

    aaaa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -129,7 +129,7 @@ class RegisterController extends Controller
     {
 
         try {
-            dd(Socialite::driver('facebook'));
+            dd(Socialite::driver('facebook')->user());
             $socialUser = Socialite::driver('facebook')->user();
             dd($socialUser);
         } catch (Exception $e) {
