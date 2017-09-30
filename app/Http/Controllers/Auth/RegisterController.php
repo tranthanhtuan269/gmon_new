a638d09e26c12738d0e23b067980f605631ea0f8
tuantt <tran.thanh.tuan269@gmail.com>
2017-09-30 11:33:46 -0700

    aaa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -129,7 +129,7 @@ class RegisterController extends Controller
     {
 
         try {
-            dd(Socialite::driver('google')->stateless()->user());
+            dd(Socialite::driver('facebook')->stateless()->user());
             $socialUser = Socialite::driver('facebook')->user();
         } catch (Exception $e) {
             return redirect('/');
