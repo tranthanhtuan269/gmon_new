61f9237ece4daf64f2a5418d8d9357c0d70bfca3
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-09 12:08:07 -0700

    aaa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -696,11 +696,17 @@ class HomeController extends Controller
     }
 
     public function sendemail(){
-        $user = [];
-        Mail::send('emails.registerUV', $user, function($message) use ($user) {
-            $message->to('tran.thanh.tuan269@gmail.com');
-            $message->subject('Mailgun Testing');
+        $dataEmail = array('email'=>'support@gmon.com.vn');
+        Mail::send('emails.registerUV', [], function($message) use ($dataEmail) {
+            $message->from('support@gmon.com.vn', 'gmon.com.vn');
+            $message->to('tran.thanh.tuan269@gmail.com')->subject('Hello Tran Thanh Tuan');
         });
+
+        // $user = [];
+        // Mail::send('emails.registerUV', $user, function($message) use ($user) {
+        //     $message->to('tran.thanh.tuan269@gmail.com');
+        //     $message->subject('Mailgun Testing');
+        // });
         dd('Mail Send Successfully');
     }
 
