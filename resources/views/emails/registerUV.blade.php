4bf0a2377c69e326f8af6e6c89d9918a0653bb1c
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-10 18:39:49 -0700

    add register email

---
+++ resources/views/emails/registerUV.blade.php
@@ -0,0 +1,3 @@
+<div style="margin-left:15px"><a href="http://gmon.vn"> Gmon.vn </a> chúc mừng bạn đã đăng ký thành công tài khoản tại <a href="http://gmon.vn">Gmon.vn </a>. Hy vọng với dịch vụ của <a href="http://gmon.vn">Gmon.vn</a>, bạn có thể sớm nhất tìm được công việc mà mình mong muốn.</div> 
+
+<div style="margin-left:15px">Mọi thắc mắc về dịch vụ hoặc cần giúp đỡ thêm, xin hãy gọi vào số <b>0243.212.1515</b> hoặc đường dây nóng <b>0961.545.115</b>. Rất hân hạnh được phục vụ quý khách! </div>
