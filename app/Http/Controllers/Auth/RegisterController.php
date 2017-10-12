66f979887bcee8abd3b34ac33b77068d7208d7c2
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-12 09:15:30 -0700

    fixbug login fb and gg was be redirect to home

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -107,7 +107,8 @@ class RegisterController extends Controller
             auth()->login($user);
         }
 
-        return redirect()->to('/#');
+        // return redirect()->to('/#');
+        return redirect()->back();
     }
 
     /**
@@ -149,6 +150,7 @@ class RegisterController extends Controller
             auth()->login($user);
         }
 
-        return redirect()->to('/#');
+        // return redirect()->to('/#');
+        return redirect()->back();
     }
 }
