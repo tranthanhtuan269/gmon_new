2f5039895017d6a68d648546edc70501d6b9d959
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-16 16:35:11 +0700

    add share google plus

--- resources/views/job/info.blade.php
+++ resources/views/job/info.blade.php
@@ -177,7 +177,7 @@
                             <a target="_blank" class="icon" href=""><i class="i1"></i></a>
                             <a target="_blank" class="icon" href=""> <i class="i2"></i></a>
                             <a target="_blank" class="icon" href=""><i class="i3"></i></a>
-                            <a target="_blank" class="icon" href=""><i class="i4"></i></a>
+                            <a target="_blank" class="icon" href="https://plus.google.com/share?url={{ $content_share['url'] }}"><i class="i4"></i></a>
                             <a target="_blank" class="icon" href="https://www.facebook.com/sharer/sharer.php?u={{ $content_share['url'] }}&display=popup"><i class="i5"></i></a>
                             <span class="hidden-xs hidden-sm">Chia sẻ qua</span>
                         </span>
