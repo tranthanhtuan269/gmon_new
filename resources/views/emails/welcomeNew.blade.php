091ac88b85db56e4e9a66b0ae17e119385a71f37
tuantt <tran.thanh.tuan269@gmail.com>
2017-11-06 11:15:17 +0700

    change style of email

--- resources/views/emails/welcomeNew.blade.php
+++ resources/views/emails/welcomeNew.blade.php
@@ -8,8 +8,8 @@
 	<h3 style="margin-left: 10px;"> Danh sách 5 công việc phù hợp với bạn </h3>
 	<div class="jobs-component">
 		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
-			<div style="width: 15%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
-			<div style="width: 82%; float: left; margin-left: 3%;">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
 			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
 			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
 			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
@@ -23,8 +23,8 @@
 			<div style="clear:both;"></div>
 		</div>
 		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
-			<div style="width: 15%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
-			<div style="width: 82%; float: left; margin-left: 3%;">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
 			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
 			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
 			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
@@ -38,8 +38,8 @@
 			<div style="clear:both;"></div>
 		</div>
 		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
-			<div style="width: 15%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
-			<div style="width: 82%; float: left; margin-left: 3%;">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
 			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
 			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
 			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
@@ -53,8 +53,8 @@
 			<div style="clear:both;"></div>
 		</div>
 		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
-			<div style="width: 15%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
-			<div style="width: 82%; float: left; margin-left: 3%;">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
 			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
 			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
 			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
@@ -68,8 +68,23 @@
 			<div style="clear:both;"></div>
 		</div>
 		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
-			<div style="width: 15%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
-			<div style="width: 82%; float: left; margin-left: 3%;">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
+			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
+			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
+			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
+			<div style="">Chi tiết tại: <a href="http://gmon.vn/job/739/ky_thuat_vien_massage">http://gmon.vn</a></div>
+			<div style="">Mô tả công việc:
+				<div>- Thực hiện các quy trình massage bấm huyệt và body cho khách hàng.</div>
+				<div>- Hỗ trợ khách hàng trong quá trình massage thư giãn.</div>
+				<div>- Giải đáp những thắc mắc của khách khi cần.</div>
+			</div>
+			</div>
+			<div style="clear:both;"></div>
+		</div>
+		<div style="margin: 10px; border: 1px solid #eee; border-radius: 5px; padding: 16px; font-size: 16px; line-height: 25px;" class="job-component">
+			<div style="width: 20%; float: left;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage"><img src="http://test.gmon.com.vn/?image=1509597960.png" class="img-responsive" alt="http://test.gmon.com.vn/?image=1509597960.png" style="width: 100%;border: 5px solid #eee;border-radius: 5px;"></a></div>
+			<div style="width: 77%; float: left; margin-left: 3%;">
 			<div style="font-size: 26px; font-weight: bold; color:#ff00a3; margin-bottom: 5px;"><a href="http://gmon.vn/job/739/ky_thuat_vien_massage" style="text-decoration: none; color:#ff00a3;">5 kỹ thuật viên massage tại <span style="text-transform: capitalize;">Relax foot Massage</span></a></div>
 			<div style="">Địa chỉ tại: Số 23 Đặng Trần Côn, Đống Đa, Hà Nội</div>
 			<div style="">Mức lương: <span style="font-size:20px; font-weight: bold; color:red;">10 - 15 triệu</span></div>
